0bfceb1e08991a72fd53bec28c28b34a4c5b64fe
Amirhossein Olyaei <amirholyaei@gmail.com>
2025-01-29 05:09:40 -0500

    Update build configuration for static export

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
-  trailingSlash: false,
+  trailingSlash: true,
   reactStrictMode: true,
   swcMinify: true,
 };
